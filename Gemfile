b8bf29c90811493fdb0e99225c63d46ba16bb65f
tranxuantruongjk <xuantruonghy823@gmail.com>
2022-08-05 09:19:03 +0700

    Chapter 6

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github){|repo| "https://github.com/#{repo}.git"}
 
 ruby "3.1.2"
 
+gem "bcrypt", "3.1.13"
 gem "bootstrap-sass", "3.4.1"
 gem "config"
 gem "importmap-rails"
---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  before_save :downcase_email
+
+  validates :name, presence: true,
+    length: {maximum: Settings.validates.name_max_length}
+
+  validates :email, presence: true,
+    length: {maximum: Settings.validates.email_max_length},
+    format: {with: Settings.validates.email_regex},
+    uniqueness: {case_sentitive: false}
+
+  validates :password, presence: true,
+    length: {minium: Settings.validates.password_min_length}
+
+  has_secure_password
+
+  private
+  def downcase_email
+    email.downcase!
+  end
+end
