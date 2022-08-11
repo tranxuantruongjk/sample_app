87d14b94a6715584042937bc063f5c28920febc9
tranxuantruongjk <xuantruonghy823@gmail.com>
2022-08-11 08:38:49 +0700

    Chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save :downcase_email
 
   validates :name, presence: true,
@@ -14,11 +16,6 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  private
-  def downcase_email
-    email.downcase!
-  end
-
   # Returns the hash digest of the given string.
   class << self
     def digest string
@@ -29,5 +26,32 @@ class User < ApplicationRecord
              end
       BCrypt::Password.create string, cost:
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  # Returns a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  private
+  def downcase_email
+    email.downcase!
   end
 end
