c6e9d6fdc601a79f468e1feec87835f96b1ef961
tranxuantruongjk <xuantruonghy823@gmail.com>
2022-08-12 17:05:22 +0700

    Chapter_11

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,21 @@
 require 'faker'
 # Create a main sample user.
 User.create!(name: "Xuan Truong",
-             email: "xuantruonghy823@gmail.com",
+             email: "example@railstutorial.org",
              password: "123456",
              password_confirmation: "123456",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
-for n in 0..99
+for n in 0..49
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name: name,
               email: email,
               password: password,
-              password_confirmation: password)
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
